9e8d1757ad8fed9d6aa8ca8486bea80070ff6372
vsm11 <vsm11@DESKTOP-5AL84MJ>
2020-03-10 16:42:54 +0300

    New try

--- Test1/ProducerConsumer.cpp
+++ Test1/ProducerConsumer.cpp
@@ -6,112 +6,109 @@
 #include <mutex>
 #include <condition_variable>
 #include <future>
+#include <vector>
 
 using namespace std;
 
-
-mutex linemtx;
-condition_variable dowork;
-
-static const uint8_t ThreadAmount = 10;
-
 class Request {
-private:
-	string request;
 public:
-	Request() : request("empty") {};
-	void SetRequest() {
-		request = "request";
-	}
+	Request() : a(0) {};
+	int a;
 };
+
 
 class Stopper {
-private:
-	bool stopId;
 public:
-	Stopper() : stopId(0) {};
-	Stopper(bool new_stopId) : stopId(new_stopId) {};
-	void SetStopId() {
-		stopId = 1;
+	Stopper() : stopSignal(false) {}
+	void setStopper(bool new_stopper) {
+		stopSignal = new_stopper;
 	}
+	bool getStopper() {
+		return stopSignal;
+	}
+private:
+	bool stopSignal;
 };
 
-Request* GetRequest(Stopper stopSignal);
-void ProcessRequest(Request* request, Stopper stopSignal);
+void ProcessRequest(Request* request, Stopper stopSignal) {
 
-class Task {
-private:
-	Request* request;
-	Stopper stopSignal;
-public:
-	Task(Request* nrequest, Stopper nstopSignal) : request(nrequest), stopSignal(nstopSignal){}
-	void exe() {
-		ProcessRequest(request, stopSignal);
-		Delete();
-	}
-	void Delete() {
-		delete[] request;
-	}
-};
+}
 
 class ThreadPool {
+public:
+	using Task = function<void()>;
+
+	explicit ThreadPool(size_t amThreads) {
+		start(amThreads);
+	}
+
+	~ThreadPool() {
+		stop();
+	}
+
+	void enqueue(Task task) {
+		{
+			unique_lock<mutex> lock{ eventmutex };
+			Tasks.emplace(move(task));
+		}
+
+		event.notify_one();
+	}
+
 private:
 	vector<thread> Pool;
-	queue<Task> TaskQueue;
-	Stopper stop;
+	condition_variable event;
+	mutex eventmutex;
+	queue<Task> Tasks;
+	Stopper stopSignal{};
+	Request request;
+	//_Binder<std::_Unforced, void (*)(Request * request, Stopper stopSignal), const std::_Ph<1>&, const std::_Ph<2>&>  f1 = bind(&ProcessRequest, placeholders::_1, placeholders::_2);
+	void start(size_t amThreads) {
+		for (auto i = 0u; i < amThreads; ++i) {
+			Pool.emplace_back([=] {
+				while (true) {
+					Task task;
+					{
+						unique_lock<mutex> lock{ eventmutex };
+						event.wait(lock, [=] { return stopSignal.getStopper() || !Tasks.empty(); });
+						if (stopSignal.getStopper()) {
+							break;
+						}
 
-public:
-	void Detach() {
-		for (int i = 0; i < ThreadAmount; ++i) {
-			Pool[i].detach();
+						task = move(Tasks.front());
+						Tasks.pop();
+					}
+					task();
+				}
+
+				});
 		}
 	}
 
-	void SetState(bool state) {
-		stop = state;
-	}
+	void stop() noexcept {
+		{
+			unique_lock<mutex> lock{ eventmutex };
+			stopSignal.setStopper(true);;
+		}
+		event.notify_all();
 
-	void AddInQueue(Request* req, Stopper stop) {
-		Task t(req, stop);
-		TaskQueue.push(t);
-		dowork.notify_one();
-	}
-
-	ThreadPool() {
-		for (uint8_t i = 0; i < ThreadAmount; ++i) {
-			Pool.emplace_back(thread(&ThreadPool::InfiniteLoop, this));
-			//Pool.back().detach();
+		for (auto& thread : Pool) {
+			thread.join();
 		}
 	}
-
-	void InfiniteLoop() {
-		while (true) {
-			{
-				unique_lock<mutex> lock(linemtx);
-				dowork.wait(lock);
-				TaskQueue.front().exe();
-			}
-		}
-	};
 };
 
-Request* GetRequest(Stopper stopSignal) {
-	return 0;
-};
-void ProcessRequest(Request* request, Stopper stopSignal) {};
+void add(Request* request, Stopper stopSignal)
+{
+	//return 5;
+}
+
+
 
 int main() {
-	ThreadPool pool;
-	auto start = chrono::high_resolution_clock::now() + chrono::seconds(5);
-	Stopper stopSignal = false;
-
-	while (chrono::high_resolution_clock::now() < start) {
-		Request* request = GetRequest(stopSignal);
-
-		if (request == NULL) {} // Add
-
-		pool.AddInQueue(request, stopSignal);
-
-	}
-	pool.Detach();
+	ThreadPool{ 10 };
+	/*auto a = 5;
+	auto f1 = bind(&ProcessRequest, placeholders::_1, placeholders::_2);
+	auto add_func = bind(&add, placeholders::_1, placeholders::_2);*/
+	return 0;
 }
